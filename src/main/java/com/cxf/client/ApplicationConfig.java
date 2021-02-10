f29411d8071b8dd7072fe723e646ec26b77cc1f8
senthil <senthilrajanr@gmail.com>
2021-02-09 18:29:46 -0600

    upgraded to cxf 3.4.2

--- src/main/java/com/cxf/client/ApplicationConfig.java
+++ src/main/java/com/cxf/client/ApplicationConfig.java
@@ -1,12 +1,10 @@
 package com.cxf.client;
 
 import com.tutorialspoint.helloworld.HelloWorldPortType;
-import com.tutorialspoint.helloworld.HelloWorldService;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.bus.extension.ExtensionManagerBus;
-import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.common.spi.GeneratedNamespaceClassLoader;
 import org.apache.cxf.common.spi.NamespaceClassCreator;
 import org.apache.cxf.endpoint.dynamic.ExceptionClassCreator;
@@ -23,18 +21,12 @@ import org.apache.cxf.wsdl.ExtensionClassLoader;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 @Configuration(proxyBeanMethods = false)
 //@ImportResource("classpath:META-INF/cxf/cxf.xml")
 public class ApplicationConfig {
 
-	/*@Bean(name=Bus.DEFAULT_BUS_ID)
+	@Bean
 	public Bus bus() {
 		final Bus bus = new ExtensionManagerBus();
 		BusFactory.setDefaultBus(bus);
@@ -47,22 +39,12 @@ public class ApplicationConfig {
 		bus.setExtension(new GeneratedNamespaceClassLoader(bus), NamespaceClassCreator.class);
 
 		return bus;
-	}*/
+	}
 
-	
+
 	@Bean("soapClient")
-	public HelloWorldPortType helloWorldClient(){
-        final Bus bus = new ExtensionManagerBus();
-		BusFactory.setDefaultBus(bus);
-
-		bus.setExtension(new WrapperHelperClassLoader(bus), WrapperHelperCreator.class);
-		bus.setExtension(new ExtensionClassLoader(bus), ExtensionClassCreator.class);
-		bus.setExtension(new ExceptionClassLoader(bus), ExceptionClassCreator.class);
-		bus.setExtension(new WrapperClassLoader(bus), WrapperClassCreator.class);
-		bus.setExtension(new FactoryClassLoader(bus), FactoryClassCreator.class);
-		bus.setExtension(new GeneratedNamespaceClassLoader(bus), NamespaceClassCreator.class);
-
-		final JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+	public HelloWorldPortType helloWorldClient(Bus bus){
+        final JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
 		factory.setBus(bus);
 		factory.setServiceClass(HelloWorldPortType.class);
 		factory.setAddress("http://localhost:8080/cxf/ws/helloworld");
